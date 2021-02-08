a188e2b9baf6c80769bad7d2eaf9f0f81b92002c
Insooooo <lapilous@naver.com>
2021-02-08 17:21:51 +0900

    Commit by Eclipse
    Test Eclipse commit function

--- multicampus2021/java_basic_workspace/Java_Project/src/client/ClientUI.java
+++ multicampus2021/java_basic_workspace/Java_Project/src/client/ClientUI.java
@@ -1,15 +1,20 @@
 package client;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class ClientUI {
 
 	public static void main(String[] args) {
-		Frame f=new Frame();
+		Frame f=new Frame("나의 채팅");
 		Panel p=new Panel();
 		Button b1=new Button("전송1");
-		TextField tf=new TextField();
+		TextField tf=new TextField(20);
 		TextArea ta=new TextArea();	
+		
+		ActionListener l=new MyButtonHandler();
+		b1.addActionListener(l);
+		
 
 		f.add(ta,BorderLayout.CENTER);
 		f.add(p,BorderLayout.SOUTH);
