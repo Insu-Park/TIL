033643623672eeb185f31fb2ff295401b556e1fd
Insooooo <lapilous@naver.com>
2021-02-08 16:04:01 +0900

    Create new project

--- multicampus2021/java_basic_workspace/Basic_ch13/src/test/generic/Test.java
+++ multicampus2021/java_basic_workspace/Basic_ch13/src/test/generic/Test.java
@@ -1,29 +1,49 @@
 package test.generic;
 
 import java.util.ArrayList;
+import java.util.List;
 public class Test {
 	public static void main(String[] args) {
-		Cat cat=new Cat();
+		
 		AnimalList<LandAnimal> zoo=new AnimalList<LandAnimal>();
+		
+		Cat cat=new Cat();
+		Dog dog=new Dog();
 		zoo.add(cat);
-
+		zoo.add(dog);
 		AnimalList<WaterAnimal> zoo2=new AnimalList<WaterAnimal>();
 		Fish fish = new Fish();
 		zoo2.add(fish);
+		
+		AnimalList.cryingAnimalList(zoo);
 	}
 
 }
 
 class AnimalList<T>{
 	ArrayList<T> al=new ArrayList<T>();//빈상자
-	public void cryingAnimalList() {//모든 동물을 울게
+	
+	static public void cryingAnimalList(AnimalList<? extends LandAnimal> al) {//모든 동물을 울게
+		
+		int index=0;
+		do{ 
+			try {
+				LandAnimal la = al.get(index);
+				la.crying();
+			}
+			catch(IndexOutOfBoundsException e) {
+				
+			}
+		}
+		while(index++<1000);
 		
 	}
 	public void add(T o) {//동물 추가
 		al.add(o);
 	}
-	public void get(){//동물 리턴
-		
+	
+	public T get(int index){//동물 리턴
+		return al.get(index);
 	}
 	public void remove() {//동물 삭제
 		
