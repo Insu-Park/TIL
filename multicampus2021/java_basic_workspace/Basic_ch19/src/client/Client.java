a21ab9b9d77d1d49f5323b43e6b3491db5193df3
Insooooo <lapilous@naver.com>
2021-02-09 16:41:44 +0900

    Update 2 java class
    Update Client and Server class

--- multicampus2021/java_basic_workspace/Basic_ch19/src/client/Client.java
+++ multicampus2021/java_basic_workspace/Basic_ch19/src/client/Client.java
@@ -1,9 +1,24 @@
 package client;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Socket s = new Socket("localhost", 1234);
+			System.out.println("server connect ok");
+			
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			out.writeUTF("Hello?");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 
 	}
 	
