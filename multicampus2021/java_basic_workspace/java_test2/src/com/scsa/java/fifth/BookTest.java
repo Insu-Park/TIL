fb18b54b535c6d6a4640d7eab608aaa406272b21
Insooooo <lapilous@naver.com>
2021-02-11 17:22:18 +0900

    Update 3 classes

--- multicampus2021/java_basic_workspace/java_test2/src/com/scsa/java/fifth/BookTest.java
+++ multicampus2021/java_basic_workspace/java_test2/src/com/scsa/java/fifth/BookTest.java
@@ -4,35 +4,17 @@ public class BookTest {
 
 	public static void main(String[] args) {
 		
-		Book b1 = new Book();
-		Book b2 = new Book();
-		Magazine m1 = new Magazine();
-		
-		b1.isbn = "21424";
-		b1.title = "Java Pro";
-		b1.author = "김하나";
-		b1.publisher = "Jaen.kr";
-		b1.price = 15000;
-		b1.desc = "기본문법";
-		
-		b2.isbn = "35355";
-		b2.title = "OOAD 분석,설계";
-		b2.author = "소나무";
-		b2.publisher = "Jaen.kr";
-		b2.price = 30000;
-		
-		m1.isbn = "35535";
-		m1.title = "Java World";
-		m1.author = "편집부";
-		m1.publisher = "androidjava.com";
-		m1.year = 2013;
-		m1.month = 2;
-		m1.price = 7000;
+		Book b1 = new Book("21424", "Java Pro", "김하나", "Jaen.kr", 15000, "기본문법");
+		Book b2 = new Book("35355", "OOAD 분석,설계", "소나무", "Jaen.kr", 30000);
+		Magazine m1 = new Magazine("35535", "Java World", "편집부", "androidjava.com", 2013, 2, 7000);
 		
 		System.out.println("*************** 도서 목록 ***************");
 		System.out.println(b1);
 		System.out.println(b2);
+		System.out.println();
+		System.out.println("*************** 잡지 목록 ***************");
 		System.out.println(m1);
+		
 	}
 
 }
