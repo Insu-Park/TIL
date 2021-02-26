73d8a82d2cd62e380dcb9173f22e9bad55df0f4b
Insooooo <lapilous@naver.com>
2021-02-26 15:44:18 +0900

    Update Web4 project

--- multicampus2021/java_basic_workspace/Web4/src/web/controller/HelloServlet.java
+++ multicampus2021/java_basic_workspace/Web4/src/web/controller/HelloServlet.java
@@ -1,6 +1,8 @@
 package web.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,14 +12,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/main")
 public class HelloServlet extends HttpServlet {
 	
+	@Override
+	public void destroy() {
+		System.out.println("deGet() 호출됨");
+		super.destroy();
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		System.out.println("doGet() 호출됨");
+		login(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		System.out.println("doPost() 호출됨");
+		login(request, response);
+	}
+	
+	private void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String id=request.getParameter("id");
+		String pw=request.getParameter("pw");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		out.append("id="+id+" pw="+pw);
+		out.close();
+
 	}
 
 }
