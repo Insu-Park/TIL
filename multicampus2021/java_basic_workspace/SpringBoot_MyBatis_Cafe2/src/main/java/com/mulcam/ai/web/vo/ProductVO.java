95ad44e97f548b15d43a290e83fbe199091d6097
Insooooo <lapilous@naver.com>
2021-03-25 17:26:33 +0900

    Update ProductVO.java

--- multicampus2021/java_basic_workspace/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/vo/ProductVO.java
+++ multicampus2021/java_basic_workspace/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/vo/ProductVO.java
@@ -2,4 +2,19 @@ package com.mulcam.ai.web.vo;
 
 public class ProductVO {
 
+	private String prod_name;
+	private int price;
+	public String getProd_name() {
+		return prod_name;
+	}
+	public void setProd_name(String prod_name) {
+		this.prod_name = prod_name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
 }
