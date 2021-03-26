c55bbe69ca3d867a03eab9b8ca3e8efe132c3525
Insooooo <lapilous@naver.com>
2021-03-26 20:28:13 +0900

    Update OrderService.java

--- multicampus2021/java_basic_workspace/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/service/OrderService.java
+++ multicampus2021/java_basic_workspace/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/service/OrderService.java
@@ -1,21 +1,83 @@
 package com.mulcam.ai.web.service;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mulcam.ai.web.dao.OrderDAO;
 import com.mulcam.ai.web.dao.OrderDAOImpl;
 import com.mulcam.ai.web.vo.OrderVO;
 
-
-
 @Service
 public class OrderService {
-	@Autowired
-	OrderDAOImpl orderDAOImpl;
 	
-	public long ordersInsert(ArrayList<OrderVO> list) throws Exception{
-		return orderDAOImpl.ordersInsert(list);
+	@Autowired
+	OrderDAOImpl orderDAO;
+	ServerSocket ss;
+	
+	public OrderService() {
+		try {
+			ss=new ServerSocket(9999);
+			
+			
+			new Thread(()-> {
+					while(true) {
+						try {
+							Socket s=ss.accept();
+							ObjectOutputStream out=new ObjectOutputStream(s.getOutputStream());
+							ObjectInputStream in=new ObjectInputStream(s.getInputStream());
+							new KitchenThread(s,in,out).start();
+						}catch(IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			).start();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-}
+	
+	//주문입력
+	public long insert(ArrayList<OrderVO> list) {
+		long order_group_no=orderDAO.insert(list);
+		//주방으로 주문 통보
+		out.writeObject();
+		return order_group_no;
+	}
+	
+	private class KitchenThread extends Thread{
+		Socket s; 
+		ObjectInputStream in;
+		ObjectOutputStream out;
+		
+		public KitchenThread(Socket s, ObjectInputStream in, ObjectOutputStream out) {
+			this.s=s;
+			this.in=in;
+			this.out=out;
+		}
+
+		@Override
+		public void run() {
+			try {
+				while(true) {				
+					in.readObject();					
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	 
+
+}//end OrderService
